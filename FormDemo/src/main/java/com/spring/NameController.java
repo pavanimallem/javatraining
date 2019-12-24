90a8f8a31cd4fa7083be3f903ff8ee97705e5211
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2019-12-24 17:55:48 +0530

    Add files via upload

---
+++ FormDemo/src/main/java/com/spring/NameController.java
@@ -0,0 +1,24 @@
+package com.spring;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class NameController {
+@RequestMapping("/name")
+public ModelAndView calc(HttpServletRequest req,HttpServletResponse res) {
+	String firstName,lastName,fullName;
+	firstName=req.getParameter("firstName");
+	lastName=req.getParameter("lastName");
+	fullName=req.getParameter("fullName");
+	fullName=firstName+ " " +lastName;
+	String result="";
+	result+=fullName;
+	return new ModelAndView("result", "result", result);	
+}
+
+}
