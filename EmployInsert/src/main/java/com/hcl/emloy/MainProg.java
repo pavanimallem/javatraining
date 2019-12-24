bbff3fb0b92d0f82530babbf5a2fef4a10a77eba
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2019-12-24 14:11:40 +0530

    Add files via upload

---
+++ EmployInsert/src/main/java/com/hcl/emloy/MainProg.java
@@ -0,0 +1,26 @@
+package com.hcl.emloy;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class MainProg {
+	public static void main(String[] args) {
+		ApplicationContext ctx=new
+				ClassPathXmlApplicationContext("applicationContext.xml");
+		DataSource dataSource=
+				(DriverManagerDataSource)ctx.getBean("dataSource");
+		JdbcTemplate jt=new JdbcTemplate(dataSource);
+		String cmd="insert into employ values(?,?,?,?,?)";
+		jt.update(cmd,new Object[]{6654,
+				                "pavani",
+				                "java",
+				                "programmer",
+		                         585666});
+		System.out.println("***record inserted***");
+	}
+
+}
