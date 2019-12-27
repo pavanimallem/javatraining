80b5fb08c7fa1595e5fe494fa0b17c209bf6a63d
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2019-12-27 10:00:08 +0530

    Add files via upload

---
+++ MavvenAnnotation/src/main/java/com/hcl/hib/EmployShow.java
@@ -0,0 +1,43 @@
+package com.hcl.hib;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class EmployShow {
+	public static void main(String[] args) {
+		Configuration cfg=new AnnotationConfiguration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session s=sf.openSession();
+		Query q=s.createQuery("from Employ");
+		q.setFirstResult(5);
+		q.setMaxResults(10);
+		List<Employ> employList=q.list();
+		for (Employ employ : employList) {
+			System.out.println("Empno "+employ.getEmpno());
+			System.out.println("Name "+employ.getName());
+			System.out.println("Dept "+employ.getDept());
+			System.out.println("Desig "+employ.getDesig());
+			System.out.println("Basic "+employ.getBasic());
+			System.out.println("-----------------");
+		}
+		q=s.createQuery("select sum(basic) from Employ");
+		List<Integer> l1=q.list();
+		System.out.println("Sum " +l1.get(0));
+		q=s.createQuery("SELECT max(basic) from Employ");
+		l1=q.list();
+		System.out.println("Max "+l1.get(0));
+		q=s.createQuery("SELECT min(basic) from Employ");
+		l1=q.list();
+		System.out.println("Min "+l1.get(0));
+		q=s.createQuery("SELECT count(basic) from Employ");
+		l1=q.list();
+		System.out.println("Count "+l1.get(0));
+	}
+
+}
---
+++ MavvenAnnotation/src/main/java/com/hcl/hib/EmployUpdate.java
@@ -0,0 +1,35 @@
+package com.hcl.hib;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class EmployUpdate {
+	public static void main(String[] args) {
+		Configuration cfg=new AnnotationConfiguration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session s=sf.openSession();
+		int empno=777;
+		Query q=s.createQuery("from Employ Where Empno="+empno);
+		List<Employ> lstEmploy=q.list();
+		Employ res=lstEmploy.get(0);
+		
+		Transaction t=s.beginTransaction();
+		res.setName("anusha");
+		res.setDept("testing");
+		res.setDesig("hadoop");
+		res.setBasic(44000);
+		
+		s.save(res);
+		t.commit();
+		System.out.println("record updated..");
+
+}
+
+}
